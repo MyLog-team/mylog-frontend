02fe4323e7677be3da189f1a162d9189dc512072
daiseek <dnjsgml0578@gmail.com>
2025-05-17 13:25:15 +0900

    chore/#1 : 개발환경세팅

---
+++ mylog-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-xl text-2xl">
+      Tailwind 적용 확인용 박스
+    </div>
+  );
+}
